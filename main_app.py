0213df3d228db3d5fad3e175dda6c87bc77c463b
Ranjith310801 <ranjith.singh@sahajgaming.in>
2024-11-16 03:15:03 +0530

    home page

---
+++ main_app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import base64
+import os
+from PIL import Image
+st.set_page_config(layout="wide")
+
+st.title('Text, Speech, Visual -> Emoji')
+
+st.image(Image.open('data/image.jpg'))
